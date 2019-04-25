c08f21959a655ece5f69d84095ff7d798b9068e3
Ryan Chynoweth <ryanchyno@outlook.com>
2019-04-25 14:52:18 -0700

    AML Deployment Script

--- code/REST/deploy_data_extractor.py
+++ code/REST/deploy_data_extractor.py
@@ -26,24 +26,16 @@ def init():
   return None
     
     
-def run(manager):
+def run(api_token):
   try: 
-    adls_connect_obj = manager.connect_adls()
-
     # api parameters
     city_ids = ['5747882', '5809844', '5799841', '5816449', '5812944', '5786882']
     city_batch = ",".join(city_ids)
     cities = ['Redmond, WA, USA','Seattle, WA, USA', 'Kirkland, WA, USA', 'Woodinville, WA, USA', 'Tacoma, WA, USA', 'Bellevue, WA, USA']
     
-    response = requests.get("http://api.openweathermap.org/data/2.5/group?id=" + city_batch + "&appid=" + manager.weather_api_token)
+    response = requests.get("http://api.openweathermap.org/data/2.5/group?id=" + city_batch + "&appid=" + api_token)
     
-    output_datetime = datetime.datetime.utcnow()
-    data_output_path = "raw/weather_data/{}/{}/{}/{}/{}/weather_data.json".format(output_datetime.year, output_datetime.month, output_datetime.day, output_datetime.hour, output_datetime.minute)
-
-    # write json file to adls
-    manager.write_json_file(adls_connect_obj, data_output_path, response.content.decode('utf8'))
-
-    return "200: Success"
+    return json.loads(response.content)
 
   except Exception as e: 
     return str(e)
@@ -60,7 +52,7 @@ with open("weather_data_extractor.py", "w") as file:
 # Create a dependencies file
 from azureml.core.conda_dependencies import CondaDependencies 
 
-myenv = CondaDependencies.create(conda_packages=['numpy', 'json', 'requests', 'datetime', 'time'])
+myenv = CondaDependencies.create(conda_packages=['numpy'], pip_packages=['requests', 'datetime', 'time'])
 
 with open("weather_env_file.yml","w") as f:
     f.write(myenv.serialize_to_string())
@@ -104,18 +96,5 @@ print(service.scoring_uri)
 import requests
 import json
 
-# we don't want to send nan to our webservice. Replace with 0. 
-test_data = pd.read_csv("data/titanic_test.csv").fillna(value=0).values
-# send a random row from the test set to score
-random_index = np.random.randint(0, len(test_data)-1)
-## we want to use double quotes in our json
-input_data = "{\"data\": [" + str(list(test_data[random_index])).replace("\'", "\"") + "]}"
-
-headers = {'Content-Type':'application/json'}
-
-resp = requests.post(service.scoring_uri, input_data, headers=headers)
-
-
-print("POST to url", service.scoring_uri)
-print("label:", test_data[random_index])
-print("prediction:", resp.text)
+data = requests.post(service.scoring_uri, manager.weather_api_token, headers={'Content-Type':'application/json'})
+data.content
--- code/REST/weather_data_extractor.py
+++ code/REST/weather_data_extractor.py
@@ -8,24 +8,16 @@ def init():
   return None
     
     
-def run(manager):
+def run(api_token):
   try: 
-    adls_connect_obj = manager.connect_adls()
-
     # api parameters
     city_ids = ['5747882', '5809844', '5799841', '5816449', '5812944', '5786882']
     city_batch = ",".join(city_ids)
     cities = ['Redmond, WA, USA','Seattle, WA, USA', 'Kirkland, WA, USA', 'Woodinville, WA, USA', 'Tacoma, WA, USA', 'Bellevue, WA, USA']
     
-    response = requests.get("http://api.openweathermap.org/data/2.5/group?id=" + city_batch + "&appid=" + manager.weather_api_token)
+    response = requests.get("http://api.openweathermap.org/data/2.5/group?id=" + city_batch + "&appid=" + api_token)
     
-    output_datetime = datetime.datetime.utcnow()
-    data_output_path = "raw/weather_data/{}/{}/{}/{}/{}/weather_data.json".format(output_datetime.year, output_datetime.month, output_datetime.day, output_datetime.hour, output_datetime.minute)
-
-    # write json file to adls
-    manager.write_json_file(adls_connect_obj, data_output_path, response.content.decode('utf8'))
-
-    return "200: Success"
+    return json.loads(response.content)
 
   except Exception as e: 
     return str(e)
